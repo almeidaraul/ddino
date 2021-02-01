7f9da925d779cfaaa2d8910dc1625d48ca4f876f
Raul Almeida <haltsimog@gmail.com>
2021-01-31 22:26:56 -0300

    add basic content

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# DDGame
-
+# Dio Dino Game
 Connectionless dino game
+
+Jogo feito durante o bootcamp da DIO - é uma cópia do jogo que o Chrome apresenta quando a conexão à internet falha.
---
+++ src/game.js
@@ -0,0 +1,21 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let isJumping = false;
+let isGameOver = false;
+let position = 0;
+
+function handleKeyUp(event) {
+	return;
+}
+
+function jump() {
+	return;
+}
+
+function createCactus() {
+	return;
+}
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
